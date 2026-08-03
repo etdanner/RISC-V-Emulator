9d87fe61b0f511376116f2e838be2be1b8ef0ab0
etdanner <ethantdanner@gmail.com>
2026-08-03 15:39:08 +0000

    added all I-format to execute + changed decode bits function

--- src/decode.c
+++ src/decode.c
@@ -1,19 +1,19 @@
 #include "decode.h"
 
 // isolates bits low to high inclusively
-static uint32_t bits(uint32_t x, int high, int low) {
+static uint32_t bits(uint32_t x, int low, int high) {
   // (shifted int so low at bit 0) & (bit mask as wide as field)
   return (x >> low) & ((1u << (high - low + 1)) - 1);
 }
 
 decoded_instr_t decode(uint32_t instr) {
   decoded_instr_t d = {0}; // init everything to 0
-  d.opcode = bits(instr, 6, 0);
-  d.rd = bits(instr, 11, 7);
-  d.funct3 = bits(instr, 14, 12);
-  d.rs1 = bits(instr, 19, 15);
-  d.rs2 = bits(instr, 24, 20);
-  d.funct7 = bits(instr, 31, 25);
+  d.opcode = bits(instr, 0, 6);
+  d.rd = bits(instr, 7, 11);
+  d.funct3 = bits(instr, 12, 14);
+  d.rs1 = bits(instr, 15, 19);
+  d.rs2 = bits(instr, 20, 24);
+  d.funct7 = bits(instr, 25, 31);
   // sign-extended, only for I-type immediate atm.
   d.imm = (int32_t)instr >> 20;
 
--- src/execute.c
+++ src/execute.c
@@ -2,15 +2,51 @@
 
 trap_cause_t execute(cpu_t *cpu, decoded_instr_t d) {
   switch (d.opcode) {
-  case 0x13:
+  case 0x13: // I-Format
     switch (d.funct3) {
     case 0x0: // addi
       reg_write(cpu, d.rd, cpu->regs[d.rs1] + d.imm);
       break;
-    default:
+    case 0x4: // xori
+      reg_write(cpu, d.rd, cpu->regs[d.rs1] ^ d.imm);
+      break;
+    case 0x6: // ori
+      reg_write(cpu, d.rd, cpu->regs[d.rs1] | d.imm);
+      break;
+    case 0x7: // andi
+      reg_write(cpu, d.rd, cpu->regs[d.rs1] & d.imm);
+      break;
+    case 0x1: { // slli
+      uint32_t shamt = d.imm & 0x1F;
+      if (d.funct7 == 0x00) {
+        reg_write(cpu, d.rd, cpu->regs[d.rs1] << shamt);
+      } else {
+        return TRAP_ILLEGAL_INSTR;
+      }
+      break;
+    }
+    case 0x5: { // braces to give scope for dec
+      uint32_t shamt = d.imm & 0x1F;
+      if (d.funct7 == 0x00) { // srli
+        reg_write(cpu, d.rd, cpu->regs[d.rs1] >> shamt);
+      } else if (d.funct7 == 0x20) { // srai
+        reg_write(cpu, d.rd, (uint32_t)((int32_t)cpu->regs[d.rs1] >> shamt));
+      } else {
+        return TRAP_ILLEGAL_INSTR;
+      }
+      break;
+    }
+    case 0x2: // slti
+      reg_write(cpu, d.rd,
+                ((int32_t)cpu->regs[d.rs1] < (int32_t)d.imm) ? 1 : 0);
+      break;
+    case 0x3: // sltiu
+      reg_write(cpu, d.rd, (cpu->regs[d.rs1] < (uint32_t)d.imm) ? 1 : 0);
+      break;
+    default: // illegal I-Format instruction
       return TRAP_ILLEGAL_INSTR;
     }
-    break; // end of immediate instrs
+    break; // end of I-Format
   default:
     return TRAP_ILLEGAL_INSTR;
   }
